b78c45aed2476da74c660196c3fece6b49fb4c7a
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2024-05-24 16:00:06 -0300

    funcionalidades bases de um jogo de luta

--- script.js
+++ script.js
@@ -9,20 +9,41 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.7
 
 class Sprite {
-    constructor({position, velocity}){
+    constructor({position, velocity, color='red', offset}){
         this.position = position
         this.velocity = velocity
+        this.width = 50
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width:100 ,
+            height:50 
+        }
+        this.color = color
+        this.isAttacking
+        this.health = 100
     }
 
     draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        //ataque
+        if (this.isAttacking){
+            c.fillStyle = 'green'
+            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        }
     }
 
     update(){
         this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -31,13 +52,24 @@ class Sprite {
             this.velocity.y = 0
         } else this.velocity.y += gravity
     }
+
+    attack(){
+        this.isAttacking = true
+        setTimeout(()=>{
+            this.isAttacking = false
+        }, 100)
+    }
 }
 const player = new Sprite({
     position: {
     x: 0,
     y: 0
-},
+    },
     velocity: {
+        x: 0,
+        y: 0
+    },
+    offset: {
         x: 0,
         y: 0
     }
@@ -51,7 +83,12 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0
-    }
+    },
+    offset: {
+        x: -50,
+        y: 0
+    },
+    color: 'blue'
 })
 
 enemy.draw()
@@ -70,6 +107,45 @@ const keys = {
         pressed: false
     }
 }
+
+function rectangularCollision({rectangle1,rectangle2}){
+    return(
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height 
+    )
+}
+
+function determineWinner({player, enemy, timerId}){
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health){
+        document.querySelector('#displayText').innerHTML = 'empate'
+    } else if (player.health > enemy.health){
+        document.querySelector('#displayText').innerHTML = 'player 1 venceu'
+    } else if (player.health < enemy.health){
+        document.querySelector('#displayText').innerHTML = 'player 2 venceu'
+    }
+}
+
+let timer = 60
+let timerId
+function decreaseTimer(){
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+
+    if (timer === 0){
+        determineWinner({player,enemy, timerId})
+        
+    }
+
+}
+
+decreaseTimer()
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -94,6 +170,32 @@ function animate(){
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){
         enemy.velocity.x = 5
     }
+
+    //colisão
+    if (rectangularCollision({
+        rectangle1: player,
+        rectangle2: enemy
+    }) &&
+        player.isAttacking){
+            player.isAttacking = false
+            enemy.health -= 20
+            document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+    }
+
+    if (rectangularCollision({
+        rectangle1: enemy,
+        rectangle2: player
+    }) &&
+        enemy.isAttacking){
+            enemy.isAttacking = false
+            player.health -= 20
+            document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    //fim de jogo por vida
+    if (enemy.health <= 0 || player.health<= 0){
+        determineWinner({player, enemy, timerId})
+    }
 }
 
 animate()
@@ -111,6 +213,9 @@ window.addEventListener('keydown', (event) => {
         case 'w':
             player.velocity.y = -20
             break
+        case ' ':
+            player.attack()
+            break
 
         case 'ArrowRight':
             keys.ArrowRight.pressed = true
@@ -122,6 +227,9 @@ window.addEventListener('keydown', (event) => {
             break
         case 'ArrowUp':
             enemy.velocity.y = -20
+            break
+        case 'ArrowDown':
+            enemy.attack()
             break
     }
 })
